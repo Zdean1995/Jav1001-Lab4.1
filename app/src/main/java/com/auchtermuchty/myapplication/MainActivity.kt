a2136dfc4f1e4cd350fbc43d810cb789f043331a
Zachary <zdean1995@gmail.com>
2023-03-07 19:29:20 -0500

    Finished whole lab.  No other commits due to issues with github.

---
+++ app/src/main/java/com/auchtermuchty/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.auchtermuchty.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+<<<<<<< HEAD
+
+
+=======
+>>>>>>> 321d8f0 (Finished project)
+    }
+}
